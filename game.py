7fd675b4ee54388707002c98566d609e5bdfa6e4
gfrancagm <gfrancagm@gmail.com>
2025-06-14 21:07:54 -0300

    começando sistema de xp e level up

--- game.py
+++ game.py
@@ -3,9 +3,12 @@ from PPlay.keyboard import *
 from PPlay.sprite import *
 from PPlay.animation import *
 from settings import *
+from PPlay.gameimage import *
 
 janela = Window(WIDTH, HEIGHT)
 teclado = janela.get_keyboard()
+bg_image = GameImage("imagens/fundo_menu.png")
+bg_image.set_position(0, 0)
 
 player_idle = Sprite('imagens/player/player_idle.png', 7)
 player_idle.set_sequence_time(0, 7, 400, True)
@@ -19,7 +22,7 @@ posYdash = HEIGHT - player_attacking.height
 
 monster_list = []
 monster = Sprite('imagens/monstro.png')
-monster.set_position(WIDTH, 100)
+monster.set_position(WIDTH/2, 500)
 monster_list.append(monster)
 
 def movimento_player():
@@ -40,7 +43,14 @@ def desenho_player():
 
 while True:        
     janela.set_background_color((44, 22, 62))
+    
+    bg_image.draw()
 
+    if xp_counter != 0 and xp_counter % 100 == 0:
+        xp_counter = 0
+        player_level += 1
+        attribute_points += 1
+        
     if teclado.key_pressed('left'):
         posXplayer -= velXplayer * janela.delta_time()
     elif teclado.key_pressed('right'):
@@ -66,6 +76,12 @@ while True:
                 monster_list.remove(monster)
                 player_state = 'idle'
                 player_attacking.stop()
+                if 1 <= player_level <= 5:
+                    xp_counter += 10
+                elif 5 < player_level <= 9: 
+                    xp_counter += 5
+                elif player_level == 10:
+                    xp_counter = 0
                 break
                                 
         if not player_attacking.is_playing():
@@ -79,7 +95,17 @@ while True:
     
     for monster in monster_list:
         monster.draw()
-    
+        
+        
+    janela.draw_text(f"XP: {int(xp_counter)}", 0, 0, size=50, color=('white'), font_name='Comic Sans')
+    janela.draw_text(f"level: {int(player_level-1)}", 0, 30, size=50, color=('white'), font_name='Comic Sans')
+    janela.draw_text(f"attribute_points: {int(attribute_points)}", 0, 60, size=50, color=('white'), font_name='Comic Sans')
+     
+    if teclado.key_pressed('esc'):            
+        monster = Sprite('imagens/monstro.png')
+        monster.set_position(WIDTH/2, 500)
+        monster_list.append(monster)
+            
     movimento_player()
     desenho_player()
 
--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 WIDTH, HEIGHT = 600, 900
-
 velXplayer = 300
 velYdash = 3000
 player_state = 'idle'
+xp_counter = 0
+attribute_points = 0
+player_level = 1
